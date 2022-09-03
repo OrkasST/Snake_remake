a6d12ed3391055ba9e2ff9bfda877fceb8f65c75
OrkasST <egor31@outlook.com>
2022-09-03 21:24:26 +0300

    --some changes

--- scripts/Utils/Map.js
+++ scripts/Utils/Map.js
@@ -23,7 +23,7 @@ export class Map {
 
   async imageLoaded() {
     console.log("Creating Map....");
-    const got = await fetch(`../../maps/${this.mapName}.json`);
+    const got = await fetch(`/Snake_remake/maps/${this.mapName}.json`);
     this.mapData = await got.json();
     this.createMap(1);
     console.log("Map Created");
