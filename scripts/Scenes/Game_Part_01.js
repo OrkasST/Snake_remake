0d3e2d7d643ee711b6ed394e7d0a5afbdff8564a
OrkasST <egor31@outlook.com>
2022-09-04 15:24:32 +0300

    ~~ 404 fixes

--- scripts/Scenes/Game_Part_01.js
+++ scripts/Scenes/Game_Part_01.js
@@ -28,7 +28,7 @@ export class Game_Part_001 {
     });
     this.objects.magic = this.objects.player.classicMagicShotTexture;
     this.textures = {
-      "map-tiles": "./Snake_remake/images/tiles.png",
+      "map-tiles": "./Snake_remake/imohbsdvdsvDBges/tiles.png",
       snake: "./Snake_remake/images/snake_10.png",
       ant: "./Snake_remake/images/ant.png",
       apple: "./Snake_remake/images/apple.png",
